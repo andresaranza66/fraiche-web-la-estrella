27537c0dbc79d8626a41d7f8911def3edb57f467
Andres Jimenez <andresjim2000@gmail.com>
2026-07-15 16:37:24 -0500

    fixing develpment variables in vercel so admin could work

--- apps/web/app/admin/page.tsx
+++ apps/web/app/admin/page.tsx
@@ -8,7 +8,7 @@ export default async function AdminPage() {
     <main className="mx-auto w-full max-w-5xl px-5 py-10">
       <div className="text-sm">
         <h1 className="font-bold text-lg border border-blue-500 rounded px-2 py-1">
-          <span className="text-blue-500 border-[color:var(--color-neutral-200)]">User:</span> {user?.primaryEmailAddress?.emailAddress ?? "no user"}
+          <span className="text-blue-500 ">User:</span> {user?.primaryEmailAddress?.emailAddress ?? "no user"}
         </h1>
       </div>
       <AdminClient />
